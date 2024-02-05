c3da599ce9727e6014e5033bef0a68be6074872f
Piterxus <piterxus@gmail.com>
2024-02-05 15:37:50 +0100

    Update ok

--- proyectoPuntal/backend/pruebasPuntal/app/Http/Controllers/Api/EmbarcacionController.php
+++ proyectoPuntal/backend/pruebasPuntal/app/Http/Controllers/Api/EmbarcacionController.php
@@ -7,6 +7,7 @@ use App\Models\Embarcacion;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Database\QueryException;
 
 class EmbarcacionController extends Controller
 {
@@ -27,9 +28,6 @@ class EmbarcacionController extends Controller
         $embarcacion->save();
 
         return response()->json($embarcacion, 201);
-
-        
-        
     }
 
     /**
@@ -40,8 +38,9 @@ class EmbarcacionController extends Controller
     //     return $embarcacion;
     // }
     public function show($matricula)
-    {$embarcacion = Embarcacion::find($matricula);
-       
+    {
+        $embarcacion = Embarcacion::find($matricula);
+
         if ($embarcacion == null) {
             return response()->json([
                 'message' => 'No se encuentra la embarcacion',
@@ -52,258 +51,121 @@ class EmbarcacionController extends Controller
             'data' => $embarcacion,
             'code' => 200
         ], 200);
-        
     }
 
     /**
      * Update the specified resource in storage.
      */
-    // public function update(Request $request, $matricula)
-    // {
-    //     // Verifica si la embarcación existe
-    //     $embarcacion = Embarcacion::find($matricula);
-
-    //     if (!$embarcacion) {
-    //         return response()->json(['error' => 'Embarcación no encontrada'], 404);
-    //     }
-
-    //     $oldData = $embarcacion->toArray();
-    //     Log::info('Datos antes de la actualización:', $oldData);
-
-    //     // Excluye 'Matricula' de la asignación masiva
-    //     $datos = $request->except('Matricula');
-
-    //     // Realiza la actualización directa en la base de datos
-    //     DB::table('embarcaciones')->where('Matricula', $matricula)->update($datos);
-
-    //     // Obtén la embarcación después de la actualización
-    //     $embarcacion = Embarcacion::find($matricula);
-    //     $newData = $embarcacion->toArray();
-
-    //     Log::info('Datos después de la actualización:', $newData);
-
-    //     // Loguea la información antes y después de la actualización
-    //     Log::info('Embarcación actualizada', [
-    //         'antes' => $oldData,
-    //         'despues' => $newData,
-    //     ]);
-
-    //     return response()->json($embarcacion, 200);
-    // }
-
-
-
-    // public function update(Request $request, Embarcacion $embarcacion)
-    // {
-    //     if (!$embarcacion) {
-    //         return response()->json(['error' => 'Embarcación no encontrada'], 404);
-    //     }
-
-    //     $oldData = $embarcacion->toArray();
-    //     Log::info('Datos antes de la actualización:', $oldData);
-
-    //     // Excluye 'Matricula' de la asignación masiva
-    //     $embarcacion->fill($request->except('Matricula'));
-
-    //     // Establece 'Matricula' manualmente
-    //     Log::info('Datos del Request:', $request->all());
-
-
-
-
-
-
-    //     // Guarda los cambios
-    //     $embarcacion->update($request->all());
-
-    //     // Obtén la embarcación después de la actualización
-    //     $embarcacion = Embarcacion::find($request->Matricula);
-    //     $newData = $embarcacion->toArray();
-
-    //     Log::info('Datos después de la actualización:', $newData);
-
-    //     // Loguea la información antes y después de la actualización
-    //     Log::info('Embarcación actualizada', [
-    //         'antes' => $oldData,
-    //         'despues' => $newData,
-    //     ]);
-
-    //     return response()->json($embarcacion, 200);
-    // }
-
-
-
-    // public function update(Request $request, Embarcacion $embarcacion)
-    // {
-    //     // Verifica si la embarcación existe
-    //     if (!$embarcacion) {
-    //         return response()->json(['error' => 'Embarcación no encontrada'], 404);
-    //     }
-
-    //     // Obtiene los datos actuales antes de la actualización
-    //     $oldData = $embarcacion->toArray();
-    //     Log::info('Datos antes de la actualización:', $oldData);
-
-    //     // Excluye 'Matricula' de la asignación masiva
-    //     // $embarcacion->fill($request->except('Matricula'));
-    //     $embarcacion->fill($request->except('Matricula'));
-
-    //     // Establece 'Matricula' manualmente
-    //     // $embarcacion->Matricula = $request->Matricula;
-
-    //     // Guarda los cambios
-    //     $embarcacion->save();
-
-    //     // Obtén la embarcación después de la actualización
-    //     $embarcacion = Embarcacion::find($request->Matricula);
-
-    //     // Verifica nuevamente si la embarcación fue encontrada
-    //     if ($embarcacion) {
-    //         // Obtiene los nuevos datos después de la actualización
-    //         $newData = $embarcacion->toArray();
-    //         Log::info('Datos después de la actualización:', $newData);
-
-    //         // Loguea la información antes y después de la actualización
-    //         Log::info('Embarcación actualizada', [
-    //             'antes' => $oldData,
-    //             'despues' => $newData,
-    //         ]);
-
-    //         return response()->json($embarcacion, 200);
-    //     } else {
-    //         return response()->json(['error' => 'Error al obtener la embarcación después de la actualización'], 500);
-    //     }
-    // }
-
-
-    // public function update(Request $request, Embarcacion $embarcacion)
-    // {
-    //     // Verifica si la embarcación existe
-    //     if (!$embarcacion) {
-    //         return response()->json(['error' => 'Embarcación no encontrada'], 404);
-    //     }
-
-    //     // Obtiene los datos actuales antes de la actualización
-    //     $oldData = $embarcacion->toArray();
-    //     Log::info('Datos antes de la actualización:', $oldData);
-
-    //     // Excluye 'Matricula' de la asignación masiva
-    //     $embarcacion->fill($request->except('Matricula'));
-    //     // $embarcacion->fill($request->all());
-    //     Log::info('Datos de la solicitud:', $request->all());
-    //     // $embarcacion->fill($request->all());
-
-    //     // Guarda los cambios
-    //     $embarcacion->save();
-
-    //     // Obtiene los datos después de la actualización
-    //     $newData = $embarcacion->toArray();
-    //     Log::info('Datos después de la actualización:', $newData);
-
-    //     // Loguea la información antes y después de la actualización
-    //     Log::info('Embarcación actualizada', [
-    //         'antes' => $oldData,
-    //         'despues' => $newData,
-    //     ]);
-
-    //     return response()->json($embarcacion, 200);
-    // }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-
-    // public function update(Request $request, Embarcacion $embarcacion)
-    // {
-    //     // Verifica si la embarcación existe
-    //     if (!$embarcacion) {
-    //         return response()->json(['error' => 'Embarcación no encontrada'], 404);
-    //     }
-    //     Log::info('Embarcación recibida:', ['Embarcación Request' => $request->Matricula]);
-    //     Log::info('Embarcación recibida:', ['Embarcación' => $embarcacion]);
-    //     Log::info('Matrícula recibida:', ['Matricula' => $embarcacion->Matricula]);
-    //     Log::info('Datos de la solicitud en el controlador:', $request->all());
-
-    //     // Obtiene los datos actuales antes de la actualización
-    //     $oldData = $embarcacion->toArray();
-    //     Log::info('Datos antes de la actualización:', $oldData);
-
-    //     // Actualiza la embarcación con los datos de la solicitud
-    //     $updateResult = $embarcacion->update($request->all());
-    //     Log::info('Datos de la solicitud:', $request->all());
-
-    //     // Verifica si la actualización fue exitosa
-    //     if ($updateResult) {
-    //         // Obtiene los datos después de la actualización
-    //         $embarcacion = Embarcacion::find($request->Matricula);
-    //         $newData = $embarcacion->toArray();
-    //         Log::info('Datos después de la actualización:', $newData);
-
-    //         // Loguea la información antes y después de la actualización
-    //         Log::info('Embarcación actualizada', [
-    //             'antes' => $oldData,
-    //             'después' => $newData,
-    //         ]);
-
-    //         return response()->json($embarcacion, 200);
-    //     } else {
-    //         return response()->json(['error' => 'Error al actualizar la embarcación'], 500);
-    //     }
-    // }
-
-
     public function update(Request $request, Embarcacion $embarcacion)
-    {\DB::enableQueryLog();
+    {
+        DB::enableQueryLog();
+
         try {
             // Verifica si la embarcación existe
             if (!$embarcacion) {
                 return response()->json(['error' => 'Embarcación no encontrada'], 404);
             }
-    
+
+            Log::info('URL de la solicitud:', ['URL' => $request->url()]);
             Log::info('Embarcación recibida:', ['Embarcación Request' => $request->Matricula]);
             Log::info('Embarcación recibida:', ['Embarcación' => $embarcacion]);
             Log::info('Matrícula recibida:', ['Matricula' => $embarcacion->Matricula]);
             Log::info('Datos de la solicitud en el controlador:', $request->all());
-    
+
             // Obtiene los datos actuales antes de la actualización
             $oldData = $embarcacion->toArray();
             Log::info('Datos antes de la actualización:', $oldData);
-    
-            // Actualiza la embarcación con los datos de la solicitud
-            Log::info('Query Log:', \DB::getQueryLog());
 
+            // Actualiza la embarcación con los datos de la solicitud
             Log::info('Datos de la solicitud Antes:', $request->all());
-       
-            // $updateResult = $embarcacion->update($request->all());
-            $embarcacion->fill($request->all());
-$updateResult = $embarcacion->save();
+
+            // Usa el método update de Eloquent para la actualización
+            $updateResult = Embarcacion::where('Matricula', $request->Matricula)->update($request->except(['Matricula', 'created_at', 'updated_at']));
 
             Log::info('Datos de la solicitud:', $request->all());
-    
+
             // Verifica si la actualización fue exitosa
             if ($updateResult) {
                 // Obtiene los datos después de la actualización
                 $embarcacion = Embarcacion::find($request->Matricula);
                 $newData = $embarcacion->toArray();
                 Log::info('Datos después de la actualización:', $newData);
-    
+
                 // Loguea la información antes y después de la actualización
                 Log::info('Embarcación actualizada', [
                     'antes' => $oldData,
                     'después' => $newData,
                 ]);
-    
+
                 return response()->json($embarcacion, 200);
             } else {
                 return response()->json(['error' => 'Error al actualizar la embarcación'], 500);
             }
+        } catch (QueryException $e) {
+            // Manejo de errores específicos de consulta SQL
+            Log::error('Error durante la actualización de la embarcación: ' . $e->getMessage());
+            return response()->json(['error' => 'Error interno del servidor'], 500);
         } catch (\Exception $e) {
-            // Manejo de errores
+            // Manejo de otros errores
             Log::error('Error durante la actualización de la embarcación: ' . $e->getMessage());
             return response()->json(['error' => 'Error interno del servidor'], 500);
         }
     }
-    
+
+
+    // public function update(Request $request, Embarcacion $embarcacion)
+    // {
+    //     DB::enableQueryLog();
+    //     try {
+    //         // Verifica si la embarcación existe
+    //         if (!$embarcacion) {
+    //             return response()->json(['error' => 'Embarcación no encontrada'], 404);
+    //         }
+    //         Log::info('URL de la solicitud:', ['URL' => $request->url()]);
+    //         Log::info('Embarcación recibida:', ['Embarcación Request' => $request->Matricula]);
+    //         Log::info('Embarcación recibida:', ['Embarcación' => $embarcacion]);
+    //         Log::info('Matrícula recibida:', ['Matricula' => $embarcacion->Matricula]);
+    //         Log::info('Datos de la solicitud en el controlador:', $request->all());
+
+    //         // Obtiene los datos actuales antes de la actualización
+    //         $oldData = $embarcacion->toArray();
+    //         Log::info('Datos antes de la actualización:', $oldData);
+
+    //         // Actualiza la embarcación con los datos de la solicitud
+    //         // Log::info('Query Log:', \DB::getQueryLog());
+
+    //         Log::info('Datos de la solicitud Antes:', $request->all());
+
+    //         // $updateResult = $embarcacion->update($request->all());
+    //         $embarcacion->fill($request->all());
+    //         $updateResult = $embarcacion->save();
+
+    //         Log::info('Datos de la solicitud:', $request->all());
+
+    //         // Verifica si la actualización fue exitosa
+    //         if ($updateResult) {
+    //             // Obtiene los datos después de la actualización
+    //             $embarcacion = Embarcacion::find($request->Matricula);
+    //             $newData = $embarcacion->toArray();
+    //             Log::info('Datos después de la actualización:', $newData);
+
+    //             // Loguea la información antes y después de la actualización
+    //             Log::info('Embarcación actualizada', [
+    //                 'antes' => $oldData,
+    //                 'después' => $newData,
+    //             ]);
+
+    //             return response()->json($embarcacion, 200);
+    //         } else {
+    //             return response()->json(['error' => 'Error al actualizar la embarcación'], 500);
+    //         }
+    //     } catch (\Exception $e) {
+    //         // Manejo de errores
+    //         Log::error('Error durante la actualización de la embarcación: ' . $e->getMessage());
+    //         return response()->json(['error' => 'Error interno del servidor'], 500);
+    //     }
+    // }
+
 
     public function destroy(Embarcacion $embarcacion)
     {
