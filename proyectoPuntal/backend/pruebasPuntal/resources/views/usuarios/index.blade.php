e596c112a5d4d70e07d63b5af8d6fef676ff7bde
Piterxus <piterxus@gmail.com>
2024-02-07 20:30:36 +0100

    Terminado estilo tabla

--- proyectoPuntal/backend/pruebasPuntal/resources/views/usuarios/index.blade.php
+++ proyectoPuntal/backend/pruebasPuntal/resources/views/usuarios/index.blade.php
@@ -1,63 +1,73 @@
 @extends('layouts.plantilla')
 
 @section('title', 'Usuarios')
-
+<link rel="stylesheet" href="{{ asset('css/tabla.css') }}">
 @section('contenido')
 
+<div class="tablaEmbarcaciones">
+    <div class="d-flex flex-row-reverse mb-2">
+        <a class="enlaceCreateEmb" href="{{ route('usuarios.create') }}">Crear usuario</a>
+    </div>
 
-<a href="{{ route('usuarios.create') }}">Crear usuario</a>
-<table id="example" class="table table-striped" style="width:100%">
-    <thead>
-        <tr>
-            <th style="display: none;">ID</th>
-                <th>Nombre</th>
-                <th>Apellido</th>
-                <th>Correo</th>
-                <th>Telefono</th>
-                <th>Rol</th>
-                <th>Estado</th>
-                <th>Email</th>
-                <th>Dirección</th>
-                <th>Imagen</th>
-                <th>Descripción</th>
-                <th>Acciones</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($usuarios as $usuario)
-        <tr>
-            <td style="display: none;">{{ $usuario->id }}</td>
-            <td>{{ $usuario->NombreCompleto }}</td>
-            <td>{{ $usuario->NombreUsuario }}</td>
-            <td>{{ $usuario->DNI }}</td>
-            <td>{{ $usuario->Telefono }}</td>
-            @if ($usuario->RolID == 1)
-            <td>Concesionario</td>
-            @elseif ($usuario->RolID == 2)
-            <td>Administrativo</td>
-            @elseif ($usuario->RolID == 3)
-            <td>Guardamuelles</td>
-            @elseif ($usuario->RolID == 4)
-            <td>Guardia Civil</td>
-            @endif
-            @if ($usuario->Habilitado == 1)
-            <td>Habilitado</td>
-            @else
-            <td>Deshabilitado</td>
-            @endif
-            <td>{{ $usuario->Email }}</td>
-            <td>{{ $usuario->Direccion }}</td>
-            <td><img src="{{ asset('images/' . $usuario->Imagen) }}" alt=""></td>
-            <td>{{ $usuario->Descripcion }}</td>
-            <td><a href="{{route('usuarios.show',['usuario'=>$usuario->UsuarioID])}}">VER</a></td>
+    <table id="example" class="table table-hover table-custom-hover rounded-3 overflow-hidden table-striped" style="width:100%">
+        <thead>
+            <tr>
+                <th [attr.colspan]="12" class="cabeceraTabla">
+                    EMBARCACIONES
+                </th>
+            </tr>
+            <tr class="cabeceraDatos">
+                <th style="display: none;">ID</th>
+                <th class="text-center">Nombre</th>
+                <th class="text-center">Apellido</th>
+                <th class="text-center">Correo</th>
+                <th class="text-center">Telefono</th>
+                <th class="text-center">Rol</th>
+                <th class="text-center">Estado</th>
+                <th class="text-center">Email</th>
+                <th class="text-center">Dirección</th>
+                <th class="text-center">Imagen</th>
+                <th class="text-center">Descripción</th>
+                <th class="text-center">Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($usuarios as $usuario)
+            <tr class=" text-center">
+                <td style="display: none;">{{ $usuario->id }}</td>
+                <td>{{ $usuario->NombreCompleto }}</td>
+                <td>{{ $usuario->NombreUsuario }}</td>
+                <td>{{ $usuario->DNI }}</td>
+                <td>{{ $usuario->Telefono }}</td>
+                @if ($usuario->RolID == 1)
+                <td>Concesionario</td>
+                @elseif ($usuario->RolID == 2)
+                <td>Administrativo</td>
+                @elseif ($usuario->RolID == 3)
+                <td>Guardamuelles</td>
+                @elseif ($usuario->RolID == 4)
+                <td>Guardia Civil</td>
+                @endif
+                @if ($usuario->Habilitado == 1)
+                <td>Habilitado</td>
+                @else
+                <td>Deshabilitado</td>
+                @endif
+                <td>{{ $usuario->Email }}</td>
+                <td>{{ $usuario->Direccion }}</td>
+                <td><img src="{{ asset('images/' . $usuario->Imagen) }}" alt=""></td>
+                <td>{{ $usuario->Descripcion }}</td>
+                <td><a href="{{route('usuarios.show',['usuario'=>$usuario->UsuarioID])}}">VER</a></td>
 
 
 
 
-        </tr>
-        @endforeach
-    </tbody>
-</table>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+
 <script type="text/javascript">
     // $(document).ready(function() {
     //     new DataTable('#example');
